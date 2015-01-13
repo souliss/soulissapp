d546abb586fa09351a5e5bf13cc7fb33e500d670
shineangelic <shineangelic@gmail.com>
2015-01-14 00:07:46 +0100

    Switch to android Studio. Base Material support, many defects to work out

--- SoulissClient/src/it/angelic/soulissclient/drawer/INavDrawerItem.java
+++
@@ -1,8 +0,0 @@
-package it.angelic.soulissclient.drawer;
-public interface INavDrawerItem {
-    public int getId();
-    public String getLabel();
-    public int getType();
-    public boolean isEnabled();
-    public boolean updateActionBarTitle();
-}
