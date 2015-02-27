95aa89187646462828967ba22927b493aabbabba
shineangelic <shineangelic@gmail.com>
2015-02-27 19:16:29 +0100

    :dadded ecoGallery

---
+++ ecoGallery/build/generated/source/buildConfig/release/us/feras/ecogallery/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package us.feras.ecogallery;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "us.feras.ecogallery";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
