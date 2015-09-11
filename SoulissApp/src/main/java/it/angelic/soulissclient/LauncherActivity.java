d67802eb677922d8eb7f56c387dc3ae7688ad92b
shineangelic <Pentium84>
2015-09-11 16:27:51 +0200

    fixed Grey-desc bug

--- SoulissApp/src/main/java/it/angelic/soulissclient/LauncherActivity.java
+++ SoulissApp/src/main/java/it/angelic/soulissclient/LauncherActivity.java
@@ -42,6 +42,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
