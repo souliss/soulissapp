2edbfa0d33eb520e6cc613b797a2ef4a383587d1
shineangelic <shineangelic@gmail.com>
2015-10-20 00:21:18 +0200

    smaller interface changes

---
+++ soulissLib/src/main/java/it/angelic/soulissclient/model/ILauncherTile.java
@@ -0,0 +1,15 @@
+package it.angelic.soulissclient.model;
+
+/**
+ * Created by Ale on 10/02/2015.
+ * <p/>
+ * Common methods to all commands
+ */
+public interface ILauncherTile {
+
+    LauncherElementEnum getComponentEnum();
+
+    ISoulissObject getLinkedObject();
+
+    //CardView getCardView();
+}
